cecfdbb95ccb1ecfafaa344587c7c65ea486de1f
Sara Monteiro <saramonteirosouza44@gmail.com>
2022-01-20 21:09:14 -0300

    Adds Server example to integrate with Event Hub

--- examples/client-minimal.py
+++ examples/client-minimal.py
@@ -19,23 +19,26 @@ if __name__ == "__main__":
         # Node objects have methods to read and write node attributes as well as browse or populate address space
         print("Children of root are: ", root.get_children())
 
+        uri = 'http://microsoft.com/Opc/OpcPlc'
+        idx = await client.get_namespace_index(uri)
         # get a specific node knowing its node id
-        #var = client.get_node(ua.NodeId(1002, 2))
-        #var = client.get_node("ns=3;i=2002")
-        #print(var)
-        #var.get_data_value() # get value of node as a DataValue object
-        #var.get_value() # get value of node as a python builtin
-        #var.set_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
-        #var.set_value(3.9) # set node value using implicit data type
+        # var = client.get_node(ua.NodeId(1002, 2))
+        # var = client.get_node("ns=3;i=2002")
+        var = await client.nodes.root.get_child(["0:Objects", f"{idx}:Objects", f"{idx}:StepUp"])
+        value = -1
+        try:
+            while True:
+                new_value =  await var.read_value()
+                if new_value != value:
+                    print("My variable", var, new_value)
+                    value = new_value
+        except KeyboardInterrupt:
+            print("Receiving has stopped.")
+        # print(var)
+        # await var.read_data_value() # get value of node as a DataValue object
+        # await var.read_value() # get value of node as a python builtin
+        # await var.write_value(ua.Variant([23], ua.VariantType.Int64)) #set node value using explicit data type
+        # await var.write_value(3.9) # set node value using implicit data type
 
-        # Now getting a variable node using its browse path
-        myvar = root.get_child(["0:Objects", "2:MyObject", "2:MyVariable"])
-        obj = root.get_child(["0:Objects", "2:MyObject"])
-        print("myvar is: ", myvar)
-        print("myobj is: ", obj)
-
-        # Stacked myvar access
-        # print("myvar is: ", root.get_children()[0].get_children()[1].get_variables()[0].get_value())
-
-    finally:
-        client.disconnect()
+if __name__ == '__main__':
+    asyncio.run(main())
