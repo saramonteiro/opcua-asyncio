d2caee03c0254c62e61e4dbdca4730a8a483beaf
Sara Monteiro <saramonteirosouza44@gmail.com>
2022-01-20 21:09:14 -0300

    Adds Server example to integrate with Event Hub

--- examples/client-minimal.py
+++ examples/client-minimal.py
@@ -16,13 +16,21 @@ async def main():
         # Node objects have methods to read and write node attributes as well as browse or populate address space
         _logger.info('Children of root are: %r', await client.nodes.root.get_children())
 
-        uri = 'http://examples.freeopcua.github.io'
+        uri = 'http://microsoft.com/Opc/OpcPlc'
         idx = await client.get_namespace_index(uri)
         # get a specific node knowing its node id
         # var = client.get_node(ua.NodeId(1002, 2))
         # var = client.get_node("ns=3;i=2002")
-        var = await client.nodes.root.get_child(["0:Objects", f"{idx}:MyObject", f"{idx}:MyVariable"])
-        print("My variable", var, await var.read_value())
+        var = await client.nodes.root.get_child(["0:Objects", f"{idx}:Objects", f"{idx}:StepUp"])
+        value = -1
+        try:
+            while True:
+                new_value =  await var.read_value()
+                if new_value != value:
+                    print("My variable", var, new_value)
+                    value = new_value
+        except KeyboardInterrupt:
+            print("Receiving has stopped.")
         # print(var)
         # await var.read_data_value() # get value of node as a DataValue object
         # await var.read_value() # get value of node as a python builtin
